b231dd218fba84c7d38e29281abbad39c3a9d9cd
LssjDavid <dquintero3269@gmail.com>
2025-09-30 16:17:58 -0400

    Fixed buggy Canva.tsx

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -1,12 +1,6 @@
-import React from 'react'
-import { useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Sphere } from '@react-three/drei';
-import ThreeWorkspace from './scripts/ThreeWorkspace';
+ import ThreeWorkspace from './scripts/ThreeWorkspace';
 
 const ThreeCanvas = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <ThreeWorkspace />
   );
